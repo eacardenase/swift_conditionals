680ead22f238aff4be9c2398686c5a23c6abbf8e
Edwin Cardenas <ecardenas@lwolf.com>
2025-03-15 12:11:29 -0500

    feat: simple if/else statements

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,12 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let population = 5422
+let message: String
+
+if population < 10000 {
+    message = "\(population) is a small town!"
+} else {
+    message = "\(population) is pretty big!"
+}
+
+print(message)
