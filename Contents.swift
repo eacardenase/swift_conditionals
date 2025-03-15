175b3896eae7b5b14cf2acf3c5058c0c64d6cf3e
Edwin Cardenas <ecardenas@lwolf.com>
2025-03-15 12:38:51 -0500

    feat: add else if statements

--- Contents.swift
+++ Contents.swift
@@ -2,10 +2,12 @@ import Cocoa
 
 let population = 5422
 let message: String
-let hasPostOffice = true
+let hasPostOffice = false
 
 if population < 10000 {
     message = "\(population) is a small town!"
+} else if population >= 10000 && population < 50000 {
+    message = "\(population) is a medium town!"
 } else {
     message = "\(population) is pretty big!"
 }
